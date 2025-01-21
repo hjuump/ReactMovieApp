34ff910774bf6d1b5407adb6277de0ce13d3e040
hjuump <164741307+hjuump@users.noreply.github.com>
2025-01-21 21:46:44 +0900

    feat: add search bar

--- screens/Search.js
+++ screens/Search.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
 
-const Search = () => (
-  <View style={styles.container}>
-    <Text>Search</Text>
-  </View>
-);
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const Container = styled.View`
+  background-color: ${props => props.theme.mainBgColor};
+`;
+const SearchBar = styled.TextInput`
+  background-color: ${props => props.theme.textInputBGColor};
+  padding: 10px 15px;
+  margin: 20px;
+  border-radius: 30px;
+`;
+const Search = () => {
+  const [query, setQuery] = useState('');
+  const onChangeText = text => setQuery(text);
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        placeholderTextColor={props => props.theme.textInputPHColor}
+        returnKeyType="search"
+        onChangeText={onChangeText}
+      />
+    </Container>
+  );
+};
+
 export default Search;
--- styled.js
+++ styled.js
@@ -1,8 +1,12 @@
 export const lightTheme = {
   mainBgColor: 'white',
   textColor: 'black',
+  textInputPHColor: 'rgba(0, 0, 0, 0.4)',
+  textInputBGColor: 'rgba(0, 0, 0, 0.07)',
 };
 export const darkTheme = {
   mainBgColor: '#1e272e',
   textColor: 'white',
+  textInputPHColor: 'rgba(255, 255, 255, 0.4)',
+  textInputBGColor: 'rgba(255, 255, 255, 0.15)',
 };
